b6ba4c31f6ef3e4ff0edf597e692f974d4e8e860
aarongertler <aaronlgertler@gmail.com>
2014-07-20 21:26:01 -0400

    chapter 7 finished

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,9 +26,28 @@ describe "UserPages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      before do
+        fill_in "Name",         with: ""
+        fill_in "Email",        with: "aaron@@gertler.com"
+        fill_in "Password",     with: "pass"
+        fill_in "Confirmation", with: "nope"
+      end
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content('blank') }
+        it { should have_content('invalid') }
+        it { should have_content('short') }
+        it { should have_content('match') }
+      end
+
     end
 
     describe "with valid information" do
@@ -42,6 +61,15 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)      
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end 
 end
